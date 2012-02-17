d7b2c379ff6cea5b75429f48fd82e1a1809eb699
Ivan Inozemtsev <ivan.inozemtsev@xored.com>
2012-02-17 14:33:15 +0700

    Added command implementation

--- plugins/com.xored.q7.extensions.ecl/src/com/xored/q7/extensions/ecl/internal/commands/GetProblemMessagesService.java
+++ plugins/com.xored.q7.extensions.ecl/src/com/xored/q7/extensions/ecl/internal/commands/GetProblemMessagesService.java
@@ -1,5 +1,8 @@
 package com.xored.q7.extensions.ecl.internal.commands;
 
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
@@ -11,6 +14,10 @@ public class GetProblemMessagesService implements ICommandService {
 	@Override
 	public IStatus service(Command command, IProcess context)
 			throws InterruptedException, CoreException {
+		for (IMarker marker : ResourcesPlugin.getWorkspace().getRoot()
+				.findMarkers(IMarker.PROBLEM, true, IResource.DEPTH_INFINITE)) {
+			context.getOutput().write(marker.getAttribute(IMarker.MESSAGE));
+		}
 		return Status.OK_STATUS;
 	}
 
